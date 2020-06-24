7213db04660d56bff5905f5ada7ed962509a8bb0
ValeryDrozd <61665987+ValeryDrozd@users.noreply.github.com>
2020-06-24 14:52:02 +0300

    Done input

--- main.cpp
+++ main.cpp
@@ -1,70 +1,72 @@
-// ConsoleApplication10.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <cstring>
 using namespace std;
+//Wav Header
+struct wav_header_t
+{
+    char chunkID[4]; //"RIFF" = 0x46464952
+    unsigned long chunkSize; //28 [+ sizeof(wExtraFormatBytes) + wExtraFormatBytes] + sum(sizeof(chunk.id) + sizeof(chunk.size) + chunk.size)
+    char format[4]; //"WAVE" = 0x45564157
+    char subchunk1ID[4]; //"fmt " = 0x20746D66
+    unsigned long subchunk1Size; //16 [+ sizeof(wExtraFormatBytes) + wExtraFormatBytes]
+    unsigned short audioFormat;
+    unsigned short numChannels;
+    unsigned long sampleRate;
+    unsigned long byteRate;
+    unsigned short blockAlign;
+    unsigned short bitsPerSample;
+    //[WORD wExtraFormatBytes;]
+    //[Extra format bytes]
+};
 
-#pragma pack(2)
-typedef struct {
-    int32_t chunkId;   // Завжди містить значення 0x52494646 (літери "RIFF")
-    int32_t chunkSize; // 36 + розмір другого підрозділу в байтах
-                       // Іншими словами 4 + (8 + SubChunk1Size) + (8 + SubChunk2Size)
-                       // Це розмір всього файла мінус 8 байтів які займають поля chunkId та chunkSize
-    int32_t format;    // Для wav-файла це завжди 0x57415645 (літери "WAVE")
-} RIFFHEADER;
+//Chunks
+struct chunk_t
+{
+    char ID[4]; //"data" = 0x61746164
+    unsigned long size;  //Chunk data bytes
+};
 
-#pragma pack(2)
-typedef struct {
-    int32_t subchunk1Id;   // Завжди 0x666d7420 – літери "fmt "
-    int32_t subchunk1Size; // Завжди 16 для аудіо PCM. Це розмір частини підрозділу, що слідує після цього числа
-    int32_t audioFormat;   // PCM = 1
-    int16_t numChannels;   // Mono = 1, Stereo = 2
-    int32_t sampleRate;    // Наприклад 44100
-    int32_t byteRate;      // == SampleRate * NumChannels * BitsPerSample/8
-    int32_t blockAlign;    // == NumChannels * BitsPerSample/8
-    int16_t bitsPerSample; // 8 bits = 8, 16 bits = 16, etc.
-} SUBCHUNK1;
+void WavReader(const char* fileName, const char* fileToSave)
+{
+    FILE* fin = fopen(fileName, "rb");
+    wav_header_t header;
+    fread(&header, sizeof(header), 1, fin);
+    chunk_t chunk;
+    while (true)
+    {
+        fread(&chunk, sizeof(chunk), 1, fin);
+        printf("%c%c%c%c\t" "%li\n", chunk.ID[0], chunk.ID[1], chunk.ID[2], chunk.ID[3], chunk.size);
+        if (*(unsigned int*)&chunk.ID == 0x61746164)
+            break;
+        fseek(fin, chunk.size, SEEK_CUR);
+    }
+    int sample_size = header.bitsPerSample / 8;
+    int samples_count = chunk.size * 8 / header.bitsPerSample;
+    printf("Samples count = %i\n", samples_count);
 
-#pragma pack(2)
-typedef struct {
-    int32_t subchunk2Id;   // 0x64617461 – літери "data"
-    int32_t subchunk2Size; // ==  * NumChannels * BitsPerSample/8, кількість байтів аудіоданих
-    int8_t* data;         // семпли
-} SUBCHUNK2; 
-
-
-RIFFHEADER head;
-SUBCHUNK1 sub1;
-SUBCHUNK2 sub2;
-int numSamples,sampleSize;
+    short int* value = new short int[samples_count];
+    memset(value, 0, sizeof(short int) * samples_count);
+    for (int i = 0; i < samples_count; i++)
+    {
+        fread(&value[i], sample_size, 1, fin);
+    }
+    cout << sizeof(value);
+    FILE* fout = fopen(fileToSave, "wb");
+    fwrite(&header, sizeof(header), 1, fout);
+    for (int i = 0; i < samples_count; i++)
+    {
+        fwrite(&value[i], sample_size, 1, fout);
+      //  fprintf(fout, "%d\n", value[i]);
+    }
+    fclose(fin);
+    fclose(fout);
+}
 
 int main()
 {
-   // cout << "Enter path to audiofile\n";
-	//char filename[256];
-	//gets_s(filename);
-	FILE* baseFile = fopen("input.wav", "rb");
-    FILE* writeFile = fopen("out.wav", "wb");
- // cout << "Enter number to scale\n";
-  //  int n;
- // cin>>n;
-    fread(&head,sizeof(head),1,baseFile);
-    fread(&sub1,sizeof(sub1),1,baseFile);
-    fread(&sub2.subchunk2Id,sizeof(int32_t),1,baseFile);
-    fread(&sub2.subchunk2Size,sizeof(int32_t),1,baseFile);
-    sampleSize = sub1.bitsPerSample / 8;
-    numSamples = sub2.subchunk2Size * 8 / sub1.bitsPerSample;
-    sub2.data = new int8_t[numSamples];
-    for(int i = 0;i<numSamples;i+=1)
-        fread(&sub2.data[i],sizeof(int8_t),1,baseFile);
-
-    fwrite(&head, sizeof(head), 1, writeFile);
-    fwrite(&sub1, sizeof(sub1), 1, writeFile);
-    fwrite(&sub2.subchunk2Id, sizeof(int32_t), 1, writeFile);
-    fwrite(&sub2.subchunk2Size, sizeof(int32_t), 1, writeFile);
-    for (int i = 0; i < numSamples; i += 1) {
-        fwrite(&sub2.data[i], sizeof(int8_t), 1, writeFile);
-    }
-   
-
+    WavReader("input.wav", "out.wav");
+    return 0;
 }
